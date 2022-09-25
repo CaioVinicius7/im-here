8864eb4c2eda85a666e355e9ae2e9c4c616837f2
Caio Vinícius <caio1525pereira@gmail.com>
2022-09-25 16:23:03 -0300

    Fundamentos - Estilizando Elementos

--- App.tsx
+++ App.tsx
@@ -2,9 +2,32 @@ import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View>
-      <Text> React </Text>
-      <Text> Native </Text>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "#131016",
+        padding: 24
+      }}
+    >
+      <Text
+        style={{
+          color: "#fdfcfe",
+          fontSize: 24,
+          fontWeight: "bold",
+          marginTop: 48
+        }}
+      >
+        Nome do evento
+      </Text>
+
+      <Text
+        style={{
+          color: "#6b6b6b",
+          fontSize: 16
+        }}
+      >
+        Domingo, 25 de novembro de 2022
+      </Text>
     </View>
   );
 }
